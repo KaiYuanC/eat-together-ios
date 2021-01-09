c6d587e64660b8c3cd4bc8805428bd1da688018f
KaiYuanC <chikaiyuan@gmail.com>
2021-01-09 13:31:10 -0500

    More UI

--- Eat2Gether/UserInterface/FoodRecommendation/FoodOptionView.swift
+++ Eat2Gether/UserInterface/FoodRecommendation/FoodOptionView.swift
@@ -9,7 +9,63 @@ import SwiftUI
 
 struct FoodOptionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            VStack(alignment: .leading) {
+                Image("foodBackground")
+                    .resizable()
+                    .frame(width: 320, height: 240)
+                Text("Sushi Restaurant 1")
+                    .font(.title2)
+                    .padding(.horizontal)
+                Text("12 Address Road, X0X 0X0\n$-$$")
+                    .font(.body)
+                    .padding(.horizontal)
+                VStack {
+                    Text("Menu includes:")
+                        .font(.system(size: 24))
+                        .foregroundColor(Constants.aqua)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding(.vertical, 12)
+                    HStack {
+                        PreferenceView(type: .dietRestriction, label: "🥑  Vegan")
+                        PreferenceView(type: .dietRestriction, label: "🥜  Nut-free")
+                        PreferenceView(type: .dietRestriction, label: "🥕  Vegetarian")
+                    }
+                    .padding(.bottom, 24)
+                }
+            }
+            .frame(width: 320)
+            .background(Color.white)
+            .cornerRadius(16)
+            .shadow(radius: 5)
+            Spacer()
+            HStack {
+                Button(action: {
+                    print("No")
+                }) {
+                    Text("❌")
+                        .padding(30)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.red, lineWidth: 2)
+                        )
+                }
+                Spacer()
+                Button(action: {
+                    print("Yes")
+                }) {
+                    Text("👍")
+                        .padding(30)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Constants.orange, lineWidth: 2)
+                        )                }
+            }
+            .frame(width: 320)
+            .padding(.bottom)
+        }
     }
 }
 
--- Eat2Gether/UserInterface/FoodRecommendation/FoodResultsView.swift
+++ Eat2Gether/UserInterface/FoodRecommendation/FoodResultsView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct FoodResultsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Top Pick")
+                .font(.system(size: 32))
+                .foregroundColor(Constants.aqua)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 24)
+            HStack (spacing: 0) {
+                ZStack {
+                    Image("foodBackground")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .clipShape(Circle())
+                    Text("75%")
+                        .font(.system(size: 52))
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(30)
+                        .background(Constants.orange)
+                        .clipShape(Circle())
+                        .opacity(0.8)
+                }
+                VStack(alignment: .leading) {
+                    Text("Sushi Restaurant 1")
+                        .font(.title2)
+                    Text("12 Address Road, X0X 0X0\n$-$$")
+                        .font(.body)
+                }
+            }
+            Text("Runner-up")
+                .font(.system(size: 32))
+                .foregroundColor(Constants.aqua)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 24)
+            HStack (spacing: 0) {
+                ZStack {
+                    Image("foodBackground")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .clipShape(Circle())
+                    Text("50%")
+                        .font(.system(size: 52))
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(30)
+                        .background(Constants.orange)
+                        .clipShape(Circle())
+                        .opacity(0.8)
+                }
+                VStack(alignment: .leading) {
+                    Text("Sushi Restaurant 2")
+                        .font(.title2)
+                    Text("12 Address Road, X0X 0X0\n$-$$")
+                        .font(.body)
+                }
+            }
+        }
     }
 }
 
